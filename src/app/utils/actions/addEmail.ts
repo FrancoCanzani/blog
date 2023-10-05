365606318e5fde783bb88ab713e113e11c2a761f
Franco Canzani <francocanzani@gmail.com>
2023-10-05 18:15:37 +0200

    git rm --cached .env

--- src/app/utils/actions/addEmail.ts
+++ src/app/utils/actions/addEmail.ts
@@ -1,5 +1,35 @@
 'use server';
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const password = process.env.MONGO_DB_PASSWORD;
+const uri = `mongodb+srv://francocanzani:${password}@cluster0.jedqy7s.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 export default async function addEmail(formData: FormData) {
   try {
     console.log(formData.get('email'));
