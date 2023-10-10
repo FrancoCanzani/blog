fffc27d1bef1cc075e8dedc10ff853e0df56608e
Franco Canzani <francocanzani@gmail.com>
2023-10-10 20:24:15 +0200

    Fixes Link error

--- src/app/components/previewPosts.tsx
+++ src/app/components/previewPosts.tsx
@@ -8,9 +8,7 @@ export function PostCard({ post }: { post: Post }) {
     <div className='mb-4 bg-gray-100 dark:bg-neutral-800 dark:text-gray-100 dark:border-gray-950 border rounded-md px-2 py-1.5'>
       <div className='flex items-center justify-between'>
         <h2 className='flex text-lg font-bold items-center justify-between hover:underline'>
-          <Link href={post.url} legacyBehavior>
-            {post.title}
-          </Link>
+          <Link href={`/posts/${post._raw.flattenedPath}`}>{post.title}</Link>
         </h2>
         <MoveUpRight size={18} />
       </div>
@@ -24,6 +22,7 @@ export function PreviewPosts() {
   // Sort date asc
   const posts = allPosts.sort((a, b) => (a.date < b.date ? 1 : -1));
   const slicedPosts = posts.slice(0, 2);
+
   return (
     <section className='w-full mb-6'>
       <h2 className='font-bold mb-4'>Latest posts</h2>
