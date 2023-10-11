67c91d6ed799c71242d14b56dc88b8f12181ca58
Franco Canzani <francocanzani@gmail.com>
2023-10-11 23:52:37 +0200

    Test

--- src/app/components/buttons/signIn.tsx
+++ src/app/components/buttons/signIn.tsx
@@ -3,7 +3,7 @@
 import { useSession, signIn, signOut } from 'next-auth/react';
 import { Github } from 'lucide-react';
 
-export default function SignIn({ postID }: { postID: string }) {
+export default function SignIn() {
   const { data: session } = useSession();
   if (session) {
     return (
@@ -18,7 +18,11 @@ export default function SignIn({ postID }: { postID: string }) {
   return (
     <button
       className='dark:bg-gray-100 flex items-center justify-center gap-0.5 dark:text-black bg-neutral-800 text-gray-100 border-gray-950 border text-sm rounded-md px-2 py-1'
-      onClick={() => signIn('github', { callbackUrl: `posts/${postID}` })}
+      onClick={() =>
+        signIn('github', {
+          callbackUrl: `https://franconotes.vercel.app/api/auth/callback/github`,
+        })
+      }
     >
       <Github size={18} />
       <span className='ml-2'>Sign in to comment</span>
--- src/app/components/commentSection.tsx
+++ src/app/components/commentSection.tsx
@@ -11,7 +11,7 @@ export default async function CommentSection({ postID }: { postID: string }) {
     <section className='mt-8'>
       <div className='pt-4 pb-2 dark:border-white border-b border-black flex items-center justify-between'>
         <h2 className='font-bold text-xl'>Comments</h2>
-        <SignIn postID={postID} />
+        <SignIn />
       </div>
       {session && <CommentForm postID={postID} />}
       <Suspense fallback={<div>Loading...</div>}>
