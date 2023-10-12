2b4212e428181d7e3934cab5cf9040337c242c17
Franco Canzani <francocanzani@gmail.com>
2023-10-12 20:44:03 +0200

    Adds spinner to delete comment button

--- src/app/components/buttons/deleteComment.tsx
+++ src/app/components/buttons/deleteComment.tsx
@@ -1,22 +1,28 @@
 'use client';
 
-import { Trash2 } from 'lucide-react';
+import { Trash2, Loader2 } from 'lucide-react';
 import { Comment } from '@/app/utils/db/models/comments';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 export default function DeleteComment({ comment }: { comment: Comment }) {
+  const [isDeleting, setIsDeleting] = useState(false);
+
   const router = useRouter();
 
   const deleteComment = async (commentID: string) => {
     try {
+      setIsDeleting(true);
       const response = await fetch(`/api/comments?commentID=${commentID}`, {
         method: 'DELETE',
       });
       router.refresh();
       if (!response.ok) {
+        setIsDeleting(false);
         throw new Error('Network response was not ok');
       }
     } catch (error) {
+      setIsDeleting(false);
       console.error(
         'There has been a problem with your fetch operation: ',
         error
@@ -32,7 +38,11 @@ export default function DeleteComment({ comment }: { comment: Comment }) {
       className='active:active:translate-y-0.5 transition-all duration-100'
       aria-label='delete'
     >
-      <Trash2 size={15} />
+      {isDeleting ? (
+        <Loader2 size={15} className='animate-spin' />
+      ) : (
+        <Trash2 size={15} />
+      )}
     </button>
   );
 }
