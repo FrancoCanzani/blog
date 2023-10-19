ac91479e730768f0d917cbe40a698213100b6f31
Franco Canzani <francocanzani@gmail.com>
2023-10-19 21:50:49 +0200

    Deletes spinner from submit button

--- src/app/components/buttons/submitButton.tsx
+++ src/app/components/buttons/submitButton.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { experimental_useFormStatus as useFormStatus } from 'react-dom';
-import { Loader2 } from 'lucide-react';
 import { cn } from '@/app/utils/cn';
 
 interface SubmitButton extends React.HTMLAttributes<HTMLButtonElement> {
@@ -16,13 +15,16 @@ export default function SubmitButton({ text, className }: SubmitButton) {
     <button
       className={cn(
         'dark:bg-gray-100 w-full flex items-center justify-center dark:text-black bg-neutral-800 text-gray-100 border-gray-950 border rounded-md px-2',
-        className
+        className,
+        {
+          'opacity-70': pending,
+        }
       )}
       type='submit'
       disabled={pending}
       aria-disabled={pending}
     >
-      {pending ? <Loader2 className='animate-spin' /> : text}
+      {text}
     </button>
   );
 }
